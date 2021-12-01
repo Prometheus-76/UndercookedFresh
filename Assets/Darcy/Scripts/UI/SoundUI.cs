778ef330f4c3dca66e9aee01b740e623fa98d229
Darcy Matheson <darcy.matheson10@gmail.com>
2021-12-01 11:40:49 +1100

    Added Sounds to Main Menu

--- Assets/Darcy/Scripts/UI/SoundUI.cs
+++ Assets/Darcy/Scripts/UI/SoundUI.cs
@@ -10,10 +10,22 @@ public class SoundUI : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip[] audioClips;
 
+    private float sceneTimer;
+
+    private void Start()
+    {
+        sceneTimer = 0f;
+    }
+
+    public void Update()
+    {
+        sceneTimer += Time.deltaTime;
+    }
+
     public void PlaySound(int index)
     {
         // If the sound index doesn't exist
-        if (index >= audioClips.Length)
+        if (index >= audioClips.Length || sceneTimer < 0.2f)
             return;
 
         // Play the sound
